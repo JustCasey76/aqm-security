3b6d21208f22e38292ea58b34c678f939ccefcd8
JustCasey76 <JustCasey76@github.com>
2025-03-14 12:36:05 -0400

    Update plugin version to 1.3.1

--- aqm-security.php
+++ aqm-security.php
@@ -3,7 +3,7 @@
  * Plugin Name: AQM Security
  * Plugin URI: https://github.com/JustCasey76/aqm-plugins/tree/main/aqm-security
  * Description: Geolocation-based security plugin using ipapi.com to control access to Formidable Forms.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: AQM
  * Author URI: https://justcasey76.com
  * Text Domain: aqm-security
@@ -21,7 +21,7 @@ if (!defined('WPINC')) {
 }
 
 // Define plugin constants
-define('AQM_SECURITY_VERSION', '1.3.0');
+define('AQM_SECURITY_VERSION', '1.3.1');
 define('AQM_SECURITY_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('AQM_SECURITY_PLUGIN_URL', plugin_dir_url(__FILE__));
 
