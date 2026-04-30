8ad06c80d89b2fe2c2a78f2c0bfa7361e663c7c0
Justin Casey <jcasey76@users.noreply.github.com>
2026-04-30 10:35:34 -0400

    chore: redirect auto-updater to AQ-Marketing org
    Future plugin updates will be served from github.com/AQ-Marketing.
    This migration release is published on JustCasey76 so existing
    WordPress installations receive it as a normal update.

--- .backup/aqm-security.php
+++ .backup/aqm-security.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: AQM Security
- * Plugin URI: https://github.com/JustCasey76/aqm-security
+ * Plugin URI: https://github.com/AQ-Marketing/aqm-security
  * Description: Geolocation-based security plugin using ipapi.com to control access to Formidable Forms.
- * Version: 1.3.5
+ * Version: 1.3.9
  * Author: AQM
  * Author URI: https://justcasey76.com
  * Text Domain: aqm-security
- * GitHub Plugin URI: https://github.com/JustCasey76/aqm-plugins
+ * GitHub Plugin URI: https://github.com/AQ-Marketing/aqm-plugins
  * GitHub Branch: main
  * Primary Branch: main
  * Release Asset: true
@@ -37,10 +37,10 @@ if (class_exists('AQM_GitHub_Updater')) {
     new AQM_GitHub_Updater([
         'slug' => plugin_basename(__FILE__),
         'proper_folder_name' => 'aqm-security',
-        'api_url' => 'https://api.github.com/repos/JustCasey76/aqm-security',
-        'raw_url' => 'https://raw.githubusercontent.com/JustCasey76/aqm-security/master',
-        'github_url' => 'https://github.com/JustCasey76/aqm-security',
-        'zip_url' => 'https://github.com/JustCasey76/aqm-security/archive/master.zip',
+        'api_url' => 'https://api.github.com/repos/AQ-Marketing/aqm-security',
+        'raw_url' => 'https://raw.githubusercontent.com/AQ-Marketing/aqm-security/master',
+        'github_url' => 'https://github.com/AQ-Marketing/aqm-security',
+        'zip_url' => 'https://github.com/AQ-Marketing/aqm-security/archive/master.zip',
         'sslverify' => true,
         'requires' => '5.6',
         'tested' => '6.4',
